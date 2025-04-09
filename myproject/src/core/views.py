107c7a4bfafdbc41a2a5eaa53cca10575f9ab438
Angeela-Lopeez <angela.lopez@tecsup.edu.pe>
2025-04-09 16:30:32 -0500

    part2

--- myproject/src/core/views.py
+++ myproject/src/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def lista_posts(request):
+    posts = Post.objects.all().order_by('-fecha_creacion')
+    return render(request, 'core/lista_posts.html', {'posts': posts})
